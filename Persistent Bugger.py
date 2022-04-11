d737957be7347f0e147aea09dfbc9ba9dc29e2f6
kulbit <I>
2022-04-11 19:49:05 +0300

    Выполнил задание

---
+++ Persistent Bugger.py
@@ -0,0 +1,28 @@
+"""
+    Write a function, persistence, that takes in a positive parameter num and returns
+its multiplicative persistence,
+which is the number of times you must multiply the digits in
+num until you reach a single digit.
+
+"""
+
+
+def persistence(n):
+    n = str(n)
+    answer = 1
+    i, number = 0, 0
+    while True:
+        while i < len(n):
+            answer *= int(n[i])
+            i = i + 1
+        number += 1
+        i = 0
+        n = answer
+        answer = 1
+        if n < 10:
+            break
+        n = str(n)
+    return number
+
+
+persistence(39)
